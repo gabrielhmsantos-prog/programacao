3a79bf2abb38e99f1cbc727f106c11db32e65a27
GABRIEL HENRICK MACIEL DOS SANTOS <gabriel_hm_santos@estudante.sesisenai.org.br>
2026-02-25 11:09:50 -0300

    sjdjs

---
+++ Atividades python 18.02/atividade 12+1.py
@@ -0,0 +1,8 @@
+numero= int(input("Digite o número: "))
+centena = numero / 100
+dezena = numero / 10
+unidade = numero
+
+print("O Número tem: ",centena, "centenas")
+print("O Número tem: ",dezena, "dezenas")
+print("O Número tem: ",unidade, "unidades") 
---
+++ Atividades python 18.02/atividade15.py
@@ -0,0 +1,7 @@
+conta = float(input("Digite o valor total da compra"))
+divconta = conta / 3
+cent = divconta - (int(divconta))
+andre = int(divconta)
+carlos = int(divconta)
+felipe = divconta + (cent *2)
+print("O valor total a pagar é: ", andre, carlos, felipe)
