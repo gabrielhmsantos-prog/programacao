b412dfcd888d13ae8732ad31fe0849cc4db35eec
gabrielhmsantos-prog <gabriel_hm_santos@estudante.sesisenai.org.br>
2026-03-11 14:19:36 +0000

    update

---
+++ Atividades python 18.02/Atividade23.py
@@ -0,0 +1,5 @@
+altura_pessoa = float(input("Digite a sua altura:"))
+sombra_pessoa = float(input("Digite a sua sombra:"))
+sombra_predio = float(input("Digite a sombra do prédio:"))
+altura_predio = (altura_pessoa * sombra_predio) /sombra_pessoa
+print("A altura do prédio é: ", altura_predio)
